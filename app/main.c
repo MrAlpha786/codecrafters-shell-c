07d7bf3c4770b9450ea3cee37027c8d6a08bcfca
Muhammad Faizan <Mr.Faizan007@pm.me>
2024-12-02 03:11:24 +0530

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int repl();
+int command_runner(char *);
+int echo(char *, int);
+void sexist(char *, int);
 
 int main()
 {
@@ -23,9 +27,69 @@ int repl()
     int l = strlen(input);
     input[l - 1] = '\0';
 
-    if (strcmp(input, "exit 0") == 0)
-      return 0;
-
-    printf("%s: command not found\n", input);
+    command_runner(input);
   }
+}
+
+int command_runner(char *input)
+{
+  int position = 0;
+  char c;
+
+  while ((c = input[position]) != ' ' && c != '\0')
+    position++;
+
+  input[position] = '\0';
+  if (strcmp(input, "echo") == 0)
+  {
+    input[position] = ' ';
+    return echo(input, position);
+  }
+  else if (strcmp(input, "exit") == 0)
+  {
+    input[position] = ' ';
+    sexist(input, position);
+  }
+
+  printf("%s: command not found\n", input);
+
+  return 0;
+}
+
+int echo(char *input, int position)
+{
+  int new_postion = 0;
+  char c;
+
+  while ((c = input[position]) == ' ')
+    position++;
+
+  while ((c = input[new_postion] = input[position]) != '\0')
+  {
+    new_postion++;
+    position++;
+  }
+
+  printf("%s\n", input);
+
+  return 0;
+}
+
+void sexist(char *input, int position)
+{
+  int new_postion = 0;
+  char c;
+
+  while ((c = input[position]) == ' ')
+    position++;
+
+  while ((c = input[new_postion] = input[position]) != '\0')
+  {
+    new_postion++;
+    position++;
+  }
+
+  int code = atoi(input);
+
+  exit(code);
 }
