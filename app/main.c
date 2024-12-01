d3cfac116aae83749dde6641fe0752203c1d42ca
Muhammad Faizan <Mr.Faizan007@pm.me>
2024-12-02 01:58:20 +0530

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
+int repl();
+
 int main()
 {
+  return repl();
+}
 
-  while (1 == 1)
+int repl()
+{
+  while (1)
   {
     printf("$ ");
     fflush(stdout);
@@ -14,10 +20,12 @@ int main()
     fgets(input, 100, stdin);
 
     // strip the last '\n'
-    input[strlen(input) - 1] = '\0';
+    int l = strlen(input);
+    input[l - 1] = '\0';
+
+    if (strcmp(input, "exit 0") == 0)
+      return 0;
 
     printf("%s: command not found\n", input);
   }
-
-  return 0;
 }
