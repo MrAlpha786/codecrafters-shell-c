a40e6619f030c1788afb47d2610ce6a6dfe5897a
Muhammad Faizan <Mr.Faizan007@pm.me>
2024-12-02 01:43:39 +0530

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+int main()
+{
   printf("$ ");
   fflush(stdout);
 
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
+
+  // strip the last '\n'
+  input[strlen(input) - 1] = '\0';
+
+  printf("%s: command not found\n", input);
+
   return 0;
 }
