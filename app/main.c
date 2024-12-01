b2ad76feeb94e1b10df9bae952872c5abc94f4d0
Muhammad Faizan <Mr.Faizan007@pm.me>
2024-12-02 04:24:45 +0530

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -2,10 +2,30 @@
 #include <string.h>
 #include <stdlib.h>
 
+typedef struct builtin_command_t
+{
+  char *name;
+  char *desc;
+  int (*runner)(char *);
+} BuiltinCommand;
+
 int repl();
-int command_runner(char *);
 int echo(char *);
-void sexist(char *);
+int sexist(char *);
+int stype(char *);
+int command_not_found(char *);
+BuiltinCommand find_command(char *);
+
+BuiltinCommand builtins[] = {
+    {name : "echo", desc : "echo is a shell builtin", runner : echo},
+    {name : "exit", desc : "exit is a shell builtin", runner : sexist},
+    {name : "type", desc : "type is a shell builtin", runner : stype}};
+
+BuiltinCommand non_existing_command = {
+  name : "",
+  desc : "not found",
+  runner : command_not_found
+};
 
 int main()
 {
@@ -27,24 +47,33 @@ int repl()
     int l = strlen(input);
     input[l - 1] = '\0';
 
-    command_runner(input);
+    BuiltinCommand com = find_command(input);
+
+    com.runner(input);
   }
 }
 
-int command_runner(char *input)
+BuiltinCommand find_command(char *command)
 {
-  if (strncmp(input, "echo", 4) == 0)
+  int len = 0;
+  char c;
+  while ((c = command[len]) != '\0' && c != ' ')
+    len++;
+
+  for (int i = 0; i < sizeof(builtins) / sizeof(builtins[0]); i++)
   {
-    if (input[4] == ' ' || input[4] == '\0')
-      return echo(input);
-  }
-  else if (strncmp(input, "exit", 4) == 0)
-  {
-    sexist(input);
+    if (strncmp(builtins[i].name, command, len) == 0)
+    {
+      return builtins[i];
+    }
   }
 
+  return non_existing_command;
+}
+
+int command_not_found(char *input)
+{
   printf("%s: command not found\n", input);
-
   return 0;
 }
 
@@ -69,9 +98,40 @@ int echo(char *input)
   return 0;
 }
 
-void sexist(char *input)
+int sexist(char *input)
 {
   int code = atoi(input);
-
   exit(code);
+
+  return 0;
+}
+
+int stype(char *input)
+{
+  // first five characters are "echo "
+  int position = 4, new_postion = 0;
+  char c;
+
+  // Remove whitespace before the string arg
+  while ((c = input[position]) == ' ')
+    position++;
+
+  while ((c = input[new_postion] = input[position]) != '\0')
+  {
+    new_postion++;
+    position++;
+  }
+
+  BuiltinCommand com = find_command(input);
+
+  if (com.name == "")
+  {
+    printf("%s: not found\n", input);
+  }
+  else
+  {
+    printf("%s\n", com.desc);
+  }
+
+  return 0;
 }
